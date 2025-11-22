80b729f74a01b7d47b7da687462639c3829f39ed
taiko <ftahacancelebi@gmail.com>
2025-11-23 00:15:53 +0300

    feat(infra): database connection

---
+++ apps/dataclaus-api/pkg/database/postgres.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewPostgresDB(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error on connecting to database: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("error on connecting to database: %w", err)
+	}
+
+	//connection pool settings
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	log.Println("Database connection successful")
+	return db, nil
+}
