961ac6aed83896cc230351b076c0f9c6d5834f37
taiko <ftahacancelebi@gmail.com>
2025-11-23 00:56:28 +0300

    feat: server is ready to go

--- apps/dataclaus-api/cmd/api/main.go
+++ apps/dataclaus-api/cmd/api/main.go
@@ -1,12 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
-}
+	adapterHttp "apps/dataclaus-api/internal/adapters/http"
+	"apps/dataclaus-api/internal/config"
+	"apps/dataclaus-api/pkg/database"
+)
 
 func main() {
-	fmt.Println(Hello("dataclaus-api"))
+	// Load config
+	cfg := config.LoadConfig()
+
+	// Connect to database
+	// we will implement in repository layer
+	db, err := database.NewPostgresDB(cfg.DatabaseDSN)
+	if err != nil {
+		log.Fatalf("Database connection failed: %v", err)
+	}
+	// escaping from go compiler
+	_ = db 
+
+	// initializing server
+	server := adapterHttp.NewServer()
+
+	// starting server with graceful shutdown
+	go func() {
+		log.Printf("Server started on port %s", cfg.ServerPort)
+		if err := server.Start(":" + cfg.ServerPort); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed to start: %v", err)
+		}
+	}()
+
+	// --- Graceful Shutdown ---
+	// handling os signals
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit // wait for signal
+
+	log.Println("System shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		server.Logger.Fatal(err)
+	}
 }
