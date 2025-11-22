c2add9b92663d38dccf45756e3754954b708e2fa
taiko <ftahacancelebi@gmail.com>
2025-11-23 00:21:52 +0300

    feat(server): middlewares implemented

---
+++ apps/dataclaus-api/internal/adapters/http/middleware.go
@@ -0,0 +1,12 @@
+package http
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func ApplyMiddlewares(e *echo.Echo) {
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+}
