e87d2f122cf912385aeddbb4deda2408957f201b
taiko <ftahacancelebi@gmail.com>
2025-11-23 17:46:55 +0300

    feat(log): zerolog added

--- apps/dataclaus-api/internal/adapters/http/middleware.go
+++ apps/dataclaus-api/internal/adapters/http/middleware.go
@@ -3,10 +3,26 @@ package http
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog/log"
 )
 
 func ApplyMiddlewares(e *echo.Echo) {
-	e.Use(middleware.Logger())
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogMethod: true,
+		LogLatency: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			log.Info().
+				Str("URI", v.URI).
+				Int("status", v.Status).
+				Str("method", v.Method).
+				Dur("latency", v.Latency).
+				Msg("request")
+
+			return nil
+		},
+	}))
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
 }
